437f41664b7aea8f85a7a6f29cb8c0b094d5611d
Lee Bousfield <ljbousfield@gmail.com>
2014-09-24 09:11:36 -0600

    Fixes

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res) {
     title: 'Sums',
     description: 'The first page! Adds up various totals in a table.',
     stats: api.wrapper.getStats(),
-    types: ['question', 'followup', 'feedback' 'note', 's_answer', 'i_answer',
+    types: ['question', 'followup', 'feedback', 'note', 's_answer', 'i_answer',
       'question_edit', 'note_edit', 's_answer_edit', 'i_answer_edit']
   });
 });
